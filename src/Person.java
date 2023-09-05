c85b10fbb012eb816f72a0dc9a8ba97cdcb0a891
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-09-05 10:36:26 -0400

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,1 +1,38 @@
-import java.util.*;
+import java.util.Calendar;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String ID;
+    private String title;
+    private int YOB;
+
+    public Person(String ID, String firstName, String lastName, String title, int YOB) {
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.title = title;
+        this.YOB = YOB;
+    }
+
+    public String fullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String formalName() {
+        return title + " " + fullName();
+    }
+
+    public int getAge() {
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        return currentYear - YOB;
+    }
+
+    public int getAge(int year) {
+        return year - YOB;
+    }
+
+    public String toCSVDataRecord() {
+        return ID + "," + firstName + "," + lastName + "," + title + "," + YOB;
+    }
+}
