5ec125683fdb01b7a3e691df98f03f8a39aa28e9
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-09-05 10:38:05 -0400

    Update PersonTest.java

--- src/PersonTest.java
+++ src/PersonTest.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class PersonTest {
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PersonTest {
+    private Person person;
+
+    @Before
+    public void setUp() {
+        person = new Person("000001", "John", "Doe", "Mr.", 1990);
+    }
+
+    @Test
+    public void testFullName() {
+        assertEquals("John Doe", person.fullName());
+    }
+
+    @Test
+    public void testFormalName() {
+        assertEquals("Mr. John Doe", person.formalName());
+    }
+
+    @Test
+    public void testGetAge() {
+        int currentYear = 2023;
+        int expectedAge = currentYear - 1990;
+        assertEquals(expectedAge, person.getAge());
+    }
+
+    @Test
+    public void testGetAgeForSpecificYear() {
+        int specificYear = 2030;
+        int expectedAge = specificYear - 1990;
+        assertEquals(expectedAge, person.getAge(specificYear));
+    }
+
+    @Test
+    public void testToCSVDataRecord() {
+        String expectedCSV = "000001,John,Doe,Mr.,1990";
+        assertEquals(expectedCSV, person.toCSVDataRecord());
+    }
 }
